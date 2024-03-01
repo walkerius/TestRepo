300d55d541739fb782126eac4bf5ae1707f29670
walkerius <walkerius@yahoo.com>
2024-03-01 16:28:25 -0600

    removal

--- BigGive/BigGive/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BigGive.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- BigGive/BigGive/Models/SubmitForm.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BigGive.Models
-{
-    public class SubmitForm
-    {
-        public int Id { get; set; }
-        public Student? International {  get; set; }
-        public string? Name { get; set; }
-    }
-}
